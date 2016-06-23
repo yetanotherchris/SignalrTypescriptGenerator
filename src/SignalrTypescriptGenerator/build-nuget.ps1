8873c4da5f95b828f38b8a205ab468b6c10c01ac
Chris S <mrshrinkray@gmail.com>
2016-06-23 20:34:34 +0100

    v1.0.12 + nuget script

--- src/SignalrTypescriptGenerator/build-nuget.ps1
+++ src/SignalrTypescriptGenerator/build-nuget.ps1
@@ -1,6 +1,7 @@
 # This could be automated via appveyor
+Write-Host "Make sure you have updated the assemblyinfo file and compiled in release mode first."
 $version = Read-Host "What is the version number?"
 $apikey = Read-Host "Enter a nuget.org API key"
 
 nuget pack .\SignalrTypescriptGenerator.nuspec  -Prop Configuration=Release -Version $version
-nuget push ".\SignalrTypescriptGenerator.$version.nupkg" $apikey
+nuget push ".\SignalrTypescriptGenerator.$version.nupkg" $apikey -Source https://www.nuget.org/api/v2/package
